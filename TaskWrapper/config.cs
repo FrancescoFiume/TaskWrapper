c1084fd7a7da89f866f0f262b9ac091af0309e4c
Francesco <franzfiume97@gmail.com>
2025-02-01 03:45:27 +0100

    config file added for binaries

---
+++ TaskWrapper/config.cs
@@ -0,0 +1,10 @@
+namespace TaskWrapper;
+
+public static class config
+{
+    public static string bashBin = "/usr/bin/bash";
+    public static string dotnetBin = "/home/ff/.dotnet/dotnet";
+    public static string pythonBin = "/usr/bin/python3";
+    public static string goBin = "/usr/bin/go";
+    public static string rustBin = "";
+}
